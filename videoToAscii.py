2c68f0cedeb7efe8e2551ddea9c890fb303b3723
Codentrain <codentrain@gmail.com>
2024-02-16 14:20:00 +0100

    patch: update imports

--- videoToAscii.py
+++ videoToAscii.py
@@ -7,13 +7,15 @@ parser = argparse.ArgumentParser(description="read camera stream and display the
 parser.add_argument("--height", type=int, default=64)
 parser.add_argument("--width", type=int, default=192)
 parser.add_argument("--rate", type=int, default=60)
+parser.add_argument("--gui", action='store_true')
 args = parser.parse_args()
 
 h = args.height
 w = args.width
 r = args.rate
+gui = args.gui
 
 
 while True:
-    print(build_img())
+    print(build_img(w, h))
     time.sleep(1 / r)
--- videoToAscii/__init__.py
+++ videoToAscii/__init__.py
@@ -1,8 +1,8 @@
-import img_converter
-import cam_access
+import videoToAscii.img_converter
+import videoToAscii.cam_access
 
 
-def build_img():
+def build_img(w, h):
     img = cam_access.capture()
     img_gray = img_converter.img_to_grey_scale(img)
     resize = img_converter.scale_img(img_gray, w, h)
